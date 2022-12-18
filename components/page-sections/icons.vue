8f64a212aec657d2de2db74f479d0a209637ed62
Your Name <you@example.com>
2022-12-18 15:54:22 +0500

    first commit

---
+++ components/page-sections/icons.vue
@@ -0,0 +1,7 @@
+<template>
+    
+  <button class="slick-prev slick-arrow" aria-label="Previous" type="button" aria-disabled="false" style="">Previous</button>  
+
+  <button class="slick-next slick-arrow" aria-label="Next" type="button" style="" aria-disabled="false">Next<button class="slick-prev slick-arrow" aria-label="Previous" type="button" aria-disabled="false" style="">Previous</button></button>
+
+</template>
