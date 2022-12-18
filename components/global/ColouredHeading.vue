b66b2ee01ac06abe18d83efe1515671d02227851
zoya rasheed <zr5530213@gmail.com>
2022-12-18 18:22:26 +0500

    Headings, PageHero

---
+++ components/global/ColouredHeading.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="heading">
+    <span class="primary">Our Future.</span>
+    <span class="secondary">Made in our</span>
+    <span class="secondary">own backyard.</span>
+  </h1>
+  <h4 class="heading">
+    <span class="secondary">I am a</span>
+    <span class="secondary">Student</span>
+  </h4>
+  <h4 class="heading">
+    <span class="secondary">I am a</span>
+    <span class="secondary">Skilled Worker</span>
+  </h4>
+  <h4 class="heading">
+    <span class="secondary">I am a</span>
+    <span class="secondary">Business</span>
+  </h4>
+</template>
---
+++ components/page-sections/PageHero.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="pagehero">
+    <div class="hero-image">
+      <figure>
+        <img src="/sites/default/files/2022-03/hero_banner_home.jpg" />
+      </figure>
+      <div class="content">
+        <div class="description">
+          <p>
+            Discover the growing opportunities in Defence Industry for talented
+            people and private businesses working to supply the Australian
+            Defence Force.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
